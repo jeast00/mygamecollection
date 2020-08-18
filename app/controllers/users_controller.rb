559906bc2710cb8ff239cd92a1b4c0a9cae0c0e5
Jonathan East <jonathan_east@ymail.com>
2020-08-18 11:45:36 -0400

    Added a get request for the login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     erb :signup
   end
 
+  #post sign up account welcome page for user
+
+
+  #get request for login page
+  get '/sessions/login' do
+    erb :'sessions/login'
+  end
+
+  #post login welcome back user and have user click to go to their account
+
 end
