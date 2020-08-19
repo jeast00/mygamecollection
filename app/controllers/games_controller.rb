2c9af34b49f459598a61b6aaccae45bc3c7fb65f
Jonathan East <jonathan_east@ymail.com>
2020-08-19 16:38:56 -0400

    Tested code and adjusted the post request to list the games on the collection page; added links to show_collection page to add, edit, delete, or go back to the users account

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,10 @@ class GamesController < ApplicationController
   get '/games/new_game' do
     erb :'games/new_game'
   end
+
+  post '/games/new_game' do
+    game = current_user.games.build(params)
+    game.save
+    redirect '/games'
+  end
 end
