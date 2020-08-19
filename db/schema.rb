59767b6a40befb340e6bed8704853873dc5af047
Jonathan East <jonathan_east@ymail.com>
2020-08-19 12:51:57 -0400

    created table info for games db; rolled back db tables and migrated both tables fresh with no users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_201706) do
+ActiveRecord::Schema.define(version: 2020_08_19_164528) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "company"
+    t.string "platform"
+    t.string "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
