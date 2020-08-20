1ea9d00d7659d11bb37c57c6e9e11b295b200bfb
Jonathan East <jonathan_east@ymail.com>
2020-08-20 13:06:46 -0400

    Was able to get the edit request to work by rearranging the edit and initial id get requests in gamescontroller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,9 +18,23 @@ class GamesController < ApplicationController
     redirect '/games'
   end
 
+  get '/games/:id/edit_game_details' do
+    @game = Game.find(params[:id])
+    erb :"/games/edit_game_details"
+  end
+
   get '/games/:id' do
     @game = Game.find(params[:id])
     erb :'/games/show_game_details'
+  end
+
+  patch '/games/:id' do
+    @game = Game.find(params[:id])
+    @game.title = params[:title]
+    @game.company = params[:company]
+    @game.platform = params[:platform]
+    @game.save
+    redirect '/games'
   end
 
 
