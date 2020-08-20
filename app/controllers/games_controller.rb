1b8432b31a5fd9f1e41743d2808b1a712182a880
Jonathan East <jonathan_east@ymail.com>
2020-08-19 23:30:57 -0400

    Added a get request for finding a game id and showing that game detail on a page; currently not working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,21 +2,21 @@ class GamesController < ApplicationController
 
   get '/games/show_collection' do
     @games = Game.all
-    erb :'games/show_collection'
+    erb :'/games/show_collection'
   end
 
   get '/games/new_game' do
-    erb :'games/new_game'
+    erb :'/games/new_game'
   end
 
   post '/games/show_collection' do
-    game = current_user.games.create(params)
+    @game = current_user.games.create(params)
     redirect '/games/show_collection'
   end
 
-  get '/games/:id' do
-    @game = Game.find(params[:id])
-    erb :'games/show_game_details'
+  get '/games/show_game_details/:id' do
+    @game = Game.find_by_id(params[:id])
+    erb :'/games/show_game_details'
   end
 
 end
