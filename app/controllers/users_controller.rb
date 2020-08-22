e00329154b9472da6bf2c03db2a305ac3f3d558c
Jonathan East <jonathan_east@ymail.com>
2020-08-22 11:47:41 -0400

    removed code from Users Controller which was placed in sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,5 @@
 class UsersController < ApplicationController
 
-  # get '/signup' do
-  #   erb :'signup'
-  # end
-  #
-  # #post sign up account welcome page for user
-  # post '/signup' do
-  #   @user = User.create(params)
-  #   session[:user_id] = @user.id
-  #   erb :'welcome_user'
-  # end
-  #
-  # #get request for login page
-  # get '/sessions/login' do
-  #   erb :'sessions/login'
-  # end
-  #
-  # #post login welcome back user and have user click to go to their account
-  # post '/sessions' do
-  #   @user = User.find_by(email: params[:email])
-  #   if @user && @user.authenticate(params[:password])
-  #     session[:user_id] = @user.id
-  #     erb :'welcome_back_user'
-  #   else
-  #     erb :'login_error'
-  #   end
-  # end
 
   get '/edit_user' do
     @user = current_user
@@ -36,11 +10,6 @@ class UsersController < ApplicationController
     @user = current_user
     erb :'user_account'
   end
-
-  # get '/user_account/edit' do
-  #   @user = current_user
-  #   erb :'edit_user'
-  # end
 
   patch '/user_account' do
     @user = current_user
